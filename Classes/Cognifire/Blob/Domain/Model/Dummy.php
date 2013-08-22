a46b9492edaff41481750c33a0e293a30798b3c7
Jacob Floyd <cognifloyd@gmail.com>
2013-08-22 16:31:10 -0400

    [TASK] Kickstart Cognifire.Blob

---
+++ Classes/Cognifire/Blob/Domain/Model/Dummy.php
@@ -0,0 +1,19 @@
+<?php
+namespace Cognifire\Blob\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Cognifire.Blob".        *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @Flow\Entity
+ */
+class Dummy {
+
+
+}
+?>
